c0db1d32fa0c9cb856fb26c718211cb68ba9b2eb
Umar Farooq <mail.umar.now@gmail.com>
2023-05-10 13:16:37 +0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+from ipinfodb.version import SDK_VERSION
+
+long_description = """
+The official Python MMDB library for IPinfo.
+
+IPinfo prides itself on being the most reliable, accurate, and in-depth source of IP address data available anywhere.
+We process terabytes of data to produce our custom IP geolocation, company, carrier and IP type data sets.
+You can visit our developer docs at https://ipinfo.io/developers.
+"""
+
+setup(
+    name="ipinfodb",
+    version=SDK_VERSION,
+    description="Official Python MMDB library for IPInfo",
+    long_description=long_description,
+    url="https://github.com/ipinfo/python-db",
+    author="IPinfo",
+    author_email="support@ipinfo.io",
+    license="Apache License 2.0",
+    packages=["ipinfodb"],
+    install_requires=["maxminddb"],
+    include_package_data=True,
+    zip_safe=False,
+)
