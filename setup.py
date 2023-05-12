5f74310cea1814e11b2aa816d24040d6858d5dae
Umar Farooq <mail.umar.now@gmail.com>
2023-05-12 10:39:51 +0500

    changed case of ipinfo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 from ipinfodb.version import SDK_VERSION
 
 long_description = """
-The official Python free database library for IPInfo.
+The official Python free database library for IPinfo.
 
 IPinfo prides itself on being the most reliable, accurate, and in-depth source of IP address data available anywhere.
 We process terabytes of data to produce our custom IP geolocation, company, carrier and IP type data sets.
@@ -13,7 +13,7 @@ You can visit our developer docs at https://ipinfo.io/developers.
 setup(
     name="ipinfodb",
     version=SDK_VERSION,
-    description="Official Python free database library for IPInfo",
+    description="Official Python free database library for IPinfo",
     long_description=long_description,
     url="https://github.com/ipinfo/python-db",
     author="IPinfo",
